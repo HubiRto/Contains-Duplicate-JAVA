ab798ffaa954b32eec125408250e18d7cb6d24f7
Hubert Rybicki <hubert.rybicki.hr@gmail.com>
2023-03-06 13:23:33 +0100

    Dodanie metody main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-    private boolean containsDuplicate(int[] nums){
+    public static void main(String[] args) {
+        int[] tab1 = {1,2,3,1};
+        int[] tab2 = {1,2,3,4};
+        int[] tab3 = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(tab3));
+    }
+
+    private static boolean containsDuplicate(int[] nums){
         Set<Integer> integerSet = new HashSet<>();
         for(int i : nums) {
             if (!integerSet.contains(i)){
